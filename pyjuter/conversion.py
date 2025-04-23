826b094155987a3fd243da2000a6ea35d3eec77c
Vedanth Padmaraman <vedanthpadmaraman0566@gmail.com>
2025-04-23 20:03:04 +0530

    Using the full AST was a HUGE overkill

--- pyjuter/conversion.py
+++ pyjuter/conversion.py
@@ -5,11 +5,6 @@ Script to convert a Python source tree to a Jupyter Notebook
 and vice-versa.
 """
 
-from ast import (
-    parse,
-    get_source_segment,
-    AST
-)
 from nbformat import (
     reads,
     writes,
@@ -19,18 +14,8 @@ from nbformat.v4 import (
     new_notebook,
     new_code_cell,
 )
+from collections.abc import Iterable
 
-class NotAstNode (AST):
-    """
-    Anything at the top level that is not parsed by `ast.parse`,
-    but which is required to preserve style.
-    """
-    def __init__ (self, start_line, start_col, end_line, end_col):
-        self.lineno = start_line
-        self.col_offset = start_col
-        self.end_lineno = end_line
-        self.end_col_offset = end_col
-        self._fields = []
 
 class Module:
     @classmethod
@@ -39,26 +24,8 @@ class Module:
         Construct from Python source code read from `source`.
         """
         res = cls ()
-        res.src = source
-        res.ast = parse (res.src)
-
-        # For every pair of consecutive top-levels that has a gap
-        # between the end of the first and start of the second, we
-        # capture whatever is in the gap using an `NotAstNode`.
-        new_body = []
-        prev_lineno = 0  # The last line that was accounted for
-        for decl in res.ast.body:
-            assert decl.col_offset == 0, (
-                "We're assuming top-level statements start at col 0 right??"
-            )
-            if decl.lineno != prev_lineno + 1:
-                nan = NotAstNode (prev_lineno + 1, 0, decl.lineno - 1, 0)
-                new_body.append (nan)
-            new_body.append (decl)
-            # TODO: Handle trailing spaces
-            prev_lineno = decl.end_lineno
-
-        res.ast.body = new_body
+        lines = source.split ("\n")
+        res.chunks = split_toplevel_stmts (lines)
         return res
 
     @classmethod
@@ -66,36 +33,60 @@ class Module:
         """
         Construct from a Jupyter Notebook read from `source`.
         """
+        res = cls ()
+        res.chunks = []
         nb = reads (source, as_version = 4)
-        cell_srcs = []
         for cell in nb.cells:
             # TODO: Verify cell type
-            cell_srcs.append (cell.source)
-        src = "\n".join (cell_srcs)
-        return cls.from_py (src)
+            res.chunks.append (cell.source)
+        return res
 
     def to_py (self) -> str:
         """
         Render as Python source.
         """
-        res = ""
-        for decl in self.ast.body:
-            try:
-                res += get_source_segment (self.src, decl) + "\n"
-            except:
-                print (decl)
-
-        return res
+        return "\n".join (self.chunks)
 
     def to_ipynb (self) -> str:
         """
         Render as Jupyter Notebook.
         """
         nb = new_notebook ()
-        for decl in self.ast.body:
-            src = get_source_segment (self.src, decl)
-            cell = new_code_cell (source = src)
+        for chunk in self.chunks:
+            cell = new_code_cell (source = chunk)
             nb.cells.append (cell)
 
-        validate (nb)        
+        validate (nb)
         return writes (nb)
+
+
+def split_toplevel_stmts (source: Iterable[str]) -> Iterable[str]:
+    """
+    Split the provided Python source into chunks on the basis of
+    blank lines between top-level statements.
+
+    The intention is for each chunk to become a cell in a
+    Jupyter Notebook.
+
+    `source` is an iterator over lines of the source
+    (excluding newline characters).
+    An iterator over the resulting chunks (without trailing
+    newlines) is returned.
+    """
+    chunk = []
+    lastline = None
+    for line in source:
+        if line:
+            if not line [0].isspace ():
+                # This is the beginning of a statement ... ->
+                if lastline == "":
+                   # -> ... after a blank line
+                   yield "\n".join (chunk)
+                   chunk = []
+
+        chunk.append (line)
+        lastline = line
+
+    # Don't forget the last chunk!
+    if chunk:
+        yield "\n".join (chunk)
